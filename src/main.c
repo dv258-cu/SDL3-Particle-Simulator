2b6329f10154552e45a7795a35639c431ae7eb07
DV_04 <68516916+DV0405@users.noreply.github.com>
2026-01-06 23:41:55 -0600

    Added an exe shortcut for debugging purposes and starting to mess with thrads

--- src/main.c
+++ src/main.c
@@ -10,6 +10,15 @@
 #define FRAMERATE 60.0
 #define PI 3.14159265358979
 
+// Basic Color Defines
+#define RED         0xFF0000FF
+#define GREEN       0x00FF00FF
+#define BLUE        0x0000FFFF
+#define BLACK       0x000000FF
+#define WHITE       0xFFFFFFFF
+#define DARK_GREY   0x333333FF
+#define LIGHT_GREY  0xAAAAAAFF
+
 // Globalize window and renderer
 SDL_Window *window      = NULL;
 SDL_Renderer *renderer  = NULL;
@@ -39,9 +48,11 @@ void Init_SimRect() {
     particle->rect->w = 100;
     particle->rect->h = 100;
 
-    particle->color = 0xFFFFFFFF;
+    particle->color = WHITE;
     particle->fill  = true;
 }
+
+
 
 void Draw_SimRect() {
     Uint8 r = (particle->color >> 24) & 0xFF;
@@ -57,17 +68,44 @@ void Draw_SimRect() {
     else {
         SDL_RenderRect(renderer, particle->rect);
     }
+
+
+
+}
+
+int frameHolder = 0;
+void Animate_SimRect(float xvel, float yvel) {
+    float xpos = particle->rect->x;
+    float ypos = particle->rect->y;
+
+    bool hitsWall = xpos >= 540 || xpos <= 0;
+
+    if (frameHolder > 10) {
+        if(hitsWall) {
+            xvel *= -1;
+            yvel *= -1;
+
+            frameHolder = 0;
+        }
+    }
+
+    particle->rect->x += xvel * deltaTime;
+    particle->rect->x += xvel * deltaTime;
+
+    frameHolder++;
 }
 
 // Called on every frame
 void update_frame() {
-    // 1. Calculate Delta Time
+    // Calculate Delta Time for framerate setting
     LAST = NOW;
     NOW = SDL_GetPerformanceCounter();
     deltaTime = (double)((NOW - LAST) / (double)SDL_GetPerformanceFrequency()) * FRAMERATE;
 
     
-
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    Animate_SimRect(1, 0);
     Draw_SimRect();
     SDL_RenderPresent(renderer);
 }
@@ -79,9 +117,21 @@ void init_dt() {
     deltaTime = 0;
 }
 
+static int UserInput(void *ptr) {
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     bool quit = false;
     SDL_Event event;
+
+    SDL_srand(0);
+
+    SDL_Thread *keybinds;
+    int threadReturnValue;
+
+    keybinds = SDL_CreateThread(UserInput, "UserInput", (void *)NULL);
+    SDL_WaitThread(keybinds, &threadReturnValue);
 
     // 1. Initialize SDL Subsystems
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -108,7 +158,10 @@ int main(int argc, char* argv[]) {
                 quit = true;
             }
             if (event.key.key == SDLK_SPACE) {
-                particle->color = particle->color * 0.9;
+                Uint32 pscolor = SDL_rand(0x00FFFFFF);
+                pscolor = (pscolor << 8) + 0xFF;
+
+                particle->color = pscolor;
             }
         }
 
